732013ee9092d6038a2bf6246f8fcd11f2cbcde7
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-10-19 14:46:30 +0200

    Ripasso scanner

--- src/it/epicode/be/Helloworld.java
+++ src/it/epicode/be/Helloworld.java
@@ -1,10 +1,27 @@
 package it.epicode.be;
 
+import java.util.Scanner;
+
 public class Helloworld {
 
 public static void main (String[] args)
 		{
 		// prints the phrase in console 
 		System.out.println("This is my first Epicode Java Project!");
+		
+
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("come ti chiami?");
+			String nome = scanner.nextLine();
+			System.out.println("Il tuo cognome?");
+			String cognome = scanner.nextLine();
+			System.out.println("La tua età");
+			String eta = scanner.nextLine();
+			System.out.println(
+					"Il tuo nome è " + nome + " " + cognome + " " + "e hai la bellezza di " + eta + " " + "anni");
+			scanner.close();
 		}
-}
+	}
+
+
